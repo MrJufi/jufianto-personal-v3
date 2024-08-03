ff04a65961f71f76ed85f036be3af060dc791a0a
MrJufi <jufianto72@gmail.com>
2024-08-03 11:11:50 +0700

    Fix Icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,90 +8,90 @@ const Header = () => {
     const [Toggle, showMenu] = useState(false);
     const [activeLink, setActiveLink] = useState('home');
 
-  return (
-    <header>
-        <nav className="container">
-            <a href="index.html" className='nav__logo'>
-                <img src={logo} alt=""/>
-            </a>
+    return (
+        <header>
+            <nav className="container">
+                <a href="index.html" className='nav__logo'>
+                    <img src={logo} alt=""/>
+                </a>
 
-            <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-                <ul className="nav__list">
-                    <li className="nav__item">
-                        <NavLink to='/'>
-                            <a href="#home" className={`${activeLink === 'home' ? 'active-link' : ''} nav__link`} 
-                                onClick={() => {
-                                    setActiveLink('home')
-                                    showMenu(!Toggle)
-                                }}>
-                                <i className="uil uil-estate nav__icon"></i>
-                                Home
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <NavLink to='/'>
+                                <a href="#home" className={`${activeLink === 'home' ? 'active-link' : ''} nav__link`} 
+                                    onClick={() => {
+                                        setActiveLink('home')
+                                        showMenu(!Toggle)
+                                    }}>
+                                    <i className="uil uil-estate nav__icon"></i>
+                                    Home
+                                </a>
+                            </NavLink> 
+                        </li>
+
+                        <li className="nav__item">
+                            <NavLink to='/about'>
+                            <a href="#about" className={`${activeLink === 'about' ? 'active-link' : ''} nav__link`} 
+                                    onClick={() => {
+                                        setActiveLink('about')
+                                        showMenu(!Toggle)
+                                    }}>
+                                <i className="uil uil-user nav__icon"></i>
+                                About
                             </a>
-                        </NavLink> 
-                    </li>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav__item">
-                        <NavLink to='/about'>
-                        <a href="#about" className={`${activeLink === 'about' ? 'active-link' : ''} nav__link`} 
-                                onClick={() => {
-                                    setActiveLink('about')
-                                    showMenu(!Toggle)
-                                }}>
-                            <i className="uil uil-user nav__icon"></i>
-                            About
-                        </a>
-                        </NavLink>
-                    </li>
+                        <li className="nav__item">
+                            <NavLink to='/qualifications'>
+                            <a href="#qualifications" className={`${activeLink === 'qualifications' ? 'active-link' : ''} nav__link`} 
+                                    onClick={() => {
+                                        setActiveLink('qualifications')
+                                        showMenu(!Toggle)    
+                                    }}>
+                                <i className="uil uil-user nav__icon"></i>
+                                Qualifications
+                            </a>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav__item">
-                        <NavLink to='/qualifications'>
-                        <a href="#qualifications" className={`${activeLink === 'qualifications' ? 'active-link' : ''} nav__link`} 
-                                onClick={() => {
-                                    setActiveLink('qualifications')
-                                    showMenu(!Toggle)    
-                                }}>
-                            <i className="uil uil-user nav__icon"></i>
-                            Qualifications
-                        </a>
-                        </NavLink>
-                    </li>
+                        <li className="nav__item">
+                            <NavLink to='/skills'>
+                            <a href="#skills" className={`${activeLink === 'skills' ? 'active-link' : ''} nav__link`} 
+                                    onClick={() => {
+                                        setActiveLink('skills')
+                                        showMenu(!Toggle)
+                                    }}>
+                                <i className="uil uil-wrench nav__icon"></i>
+                                Skills
+                            </a>
+                            </NavLink>
+                        </li>
 
-                    <li className="nav__item">
-                        <NavLink to='/skills'>
-                        <a href="#skills" className={`${activeLink === 'skills' ? 'active-link' : ''} nav__link`} 
-                                onClick={() => {
-                                    setActiveLink('skills')
-                                    showMenu(!Toggle)
-                                }}>
-                            <i className="uil uil-wrench nav__icon"></i>
-                            Skills
-                        </a>
-                        </NavLink>
-                    </li>
+                        <li className="nav__item">
+                            <NavLink to='/portfolio '>
+                            <a href="#portfolio" className={`${activeLink === 'portfolio' ? 'active-link' : ''} nav__link`} 
+                                    onClick={() => {
+                                        setActiveLink('portfolio')
+                                        showMenu(!Toggle)
+                                    }}>
+                                <i className="uil uil-briefcase nav__icon"></i>
+                                Portfolio
+                            </a>
+                            </NavLink>
+                        </li>
+                        
+                        <i className="uil uil-times nav__close" onClick={() => {showMenu(!Toggle)}}></i>
+                    </ul>
+                </div>
 
-                    <li className="nav__item">
-                        <NavLink to='/portfolio '>
-                        <a href="#portfolio" className={`${activeLink === 'portfolio' ? 'active-link' : ''} nav__link`} 
-                                onClick={() => {
-                                    setActiveLink('portfolio')
-                                    showMenu(!Toggle)
-                                }}>
-                            <i className="uil uil-briefcase nav__icon"></i>
-                            Portfolio
-                        </a>
-                        </NavLink>
-                    </li>
-                    
-                    <i className="uil uil-times nav__close" onClick={() => {showMenu(!Toggle)}}></i>
-                </ul>
-            </div>
-
-            <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                <i className="uil uil-apps"></i>
-            </div>
-        </nav>
-    </header>
-  )
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i className="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
--- src/components/skills/Softskill.jsx
+++ src/components/skills/Softskill.jsx
@@ -3,81 +3,81 @@ import React from 'react'
 
 const Softskill = () => {
   return (
-<div className="skills__content animate__animated animate__fadeInUpBig">
-    <h3 className="skills__title">Soft Skills</h3>
+    <div className="skills__content animate__animated animate__fadeInUpBig">
+        <h3 className="skills__title">Soft Skills</h3>
 
-    <div className="skills__box">
-        <div className="skills__group">
-            <div className="skills__data">
-                <i class="uil uil-brain"></i>
-                <div>
-                    <h3 className="skills__name">Problem Solving</h3>
-                    <span className="skills__level">
-                        I am able to find a solution to a problem based on 
-                        data and a little bit of analysis.
-                    </span>
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class="uil uil-brain"></i>
+                    <div>
+                        <h3 className="skills__name">Problem Solving</h3>
+                        <span className="skills__level">
+                            I am able to find a solution to a problem based on 
+                            data and a little bit of analysis.
+                        </span>
+                    </div>
                 </div>
-            </div>
 
-            <div className="skills__data">
-                <i class='bx bxs-analyse' ></i>
-                <div>
-                    <h3 className="skills__name">Adaptability</h3>
-                    <span className="skills__level">
-                        I am able to adapt fast in any situation, like the new work 
-                        environment or meeting with new people I haven't met before
-                    </span>
+                <div className="skills__data">
+                    <i class='bx bxs-analyse' ></i>
+                    <div>
+                        <h3 className="skills__name">Adaptability</h3>
+                        <span className="skills__level">
+                            I am able to adapt fast in any situation, like the new work 
+                            environment or meeting with new people I haven't met before
+                        </span>
+                    </div>
                 </div>
-            </div>
 
-            <div className="skills__data">
-                    <i class="uil uil-lightbulb"></i>
+                <div className="skills__data">
+                    <i class='bx bx-extension'></i>
                     <div>
                         <h3 className="skills__name">Critical Thinking</h3>
                         <span className="skills__level">
                             I am able to solve problems through critical thinking and enjoy working in an analytical environment.
                         </span>
                     </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+                    <div className="skills__data">
+                        <i class='bx bx-user-voice' ></i>
+                        <div>
+                            <h3 className="skills__name">Teamwork</h3>
+                            <span className="skills__level">
+                                I am able to work with others who have the same 
+                                passion and goal with me, and we together can meet
+                                the expected result
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i class="uil uil-lightbulb"></i>
+                        <div>
+                            <h3 className="skills__name">Creativity</h3>
+                            <span className="skills__level">
+                                I am able to solve problems with a creative way and I love
+                                to work in a design environment 
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i class='bx bxs-message-rounded-dots'></i>
+                        <div>
+                            <h3 className="skills__name">Communication</h3>
+                            <span className="skills__level">
+                                I am able to solve problems through effective Communication
+                                and enjoy working in a collaborative environment
+                            </span>
+                        </div>
+                    </div>
             </div>
         </div>
-
-        <div className="skills__group">
-                <div className="skills__data">
-                    <i class='bx bx-user-voice' ></i>
-                    <div>
-                        <h3 className="skills__name">Teamwork</h3>
-                        <span className="skills__level">
-                            I am able to work with others who have the same 
-                            passion and goal with me, and we together can meet
-                            the expected result
-                        </span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class="uil uil-lightbulb"></i>
-                    <div>
-                        <h3 className="skills__name">Creativity</h3>
-                        <span className="skills__level">
-                            I am able to solve problems with a creative way and I love
-                            to work in a design environment 
-                        </span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class="uil uil-lightbulb"></i>
-                    <div>
-                        <h3 className="skills__name">Communication</h3>
-                        <span className="skills__level">
-                            I am able to solve problems through effective Communication
-                            and enjoy working in a collaborative environment
-                        </span>
-                    </div>
-                </div>
-        </div>
     </div>
-</div>
   )
 }
 
